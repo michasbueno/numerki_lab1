309e8818659380f8eb3e16936de4cfed53ea906f
mkaniowski <michal.kaniowski2001@gmail.com>
2021-10-10 17:54:19 +0200

    fib

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def cylinder_area(r: float, h: float):
+import math
+import numpy as np
+
+def cylinder_area(r: float, h: float) -> float:
     """Obliczenie pola powierzchni walca.
     Szczegółowy opis w zadaniu 1.
 
@@ -9,10 +12,13 @@ def cylinder_area(r: float, h: float):
     Returns:
     float: pole powierzchni walca
     """
-    return None
+    if r > 0 and h > 0:
+        return math.pi * r * r * 2 + 2 * math.pi * r * h
+    else:
+        return math.nan
 
 
-def fib(n: int):
+def fib(n: int) -> np.ndarray:
     """Obliczenie pierwszych n wyrazów ciągu Fibonnaciego.
     Szczegółowy opis w zadaniu 3.
 
@@ -22,7 +28,14 @@ def fib(n: int):
     Returns:
     np.ndarray: wektor n pierwszych wyrazów ciągu Fibonnaciego.
     """
-    return None
+    output = np.ndarray(shape=(1, 2), dtype=int)
+    output[0, 0], output[0, 1] = 0, 1
+    if n > 2:
+        output.resize((1, n))
+        output[0, 0], output[0, 1] = 0, 1
+        for i in range(2, n):
+            output[0, i] = output[0, i-1] + output[0, i-2]
+    return output[0, :n]
 
 
 def matrix_calculations(a: float):
@@ -52,3 +65,5 @@ def custom_matrix(m: int, n: int):
     np.ndarray: macierz zgodna z opisem z zadania 7.
     """
     return None
+
+print(fib(50))
